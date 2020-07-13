16a9b566edd3b91ecd9d48202211d7c862e71d95
Abdul <abdul@uitouxsolutions.com>
2020-07-13 12:09:35 +0530

    Update 2019_10_17_184104_roles_u1.php

--- src/database/migrations/2019_10_17_184104_roles_u1.php
+++ src/database/migrations/2019_10_17_184104_roles_u1.php
@@ -11,67 +11,79 @@ class RolesU1 extends Migration {
 	 * @return void
 	 */
 	public function up() {
-		Schema::table('users', function (Blueprint $table) {
-			$table->increments('id')->change();
-		});
+		if (Schema::hasTable('users')) {
+			Schema::table('users', function (Blueprint $table) {
+				$table->increments('id')->change();
+			});
+		}
 
 		// Create table for storing roles
-		Schema::create('roles', function (Blueprint $table) {
-			$table->increments('id');
-			$table->string('name')->unique();
-			$table->string('display_name')->nullable();
-			$table->string('description')->nullable();
-			$table->timestamps();
-		});
+		if (!Schema::hasTable('roles')) {
+			Schema::create('roles', function (Blueprint $table) {
+				$table->increments('id');
+				$table->string('name')->unique();
+				$table->string('display_name')->nullable();
+				$table->string('description')->nullable();
+				$table->timestamps();
+			});
+		}
 
 		// Create table for associating roles to users (Many-to-Many)
-		Schema::create('role_user', function (Blueprint $table) {
-			$table->integer('user_id')->unsigned();
-			$table->integer('role_id')->unsigned();
+		if (!Schema::hasTable('role_user')) {
+			Schema::create('role_user', function (Blueprint $table) {
+				$table->integer('user_id')->unsigned();
+				$table->integer('role_id')->unsigned();
 
-			$table->foreign('user_id')->references('id')->on('users')
-				->onUpdate('cascade')->onDelete('cascade');
-			$table->foreign('role_id')->references('id')->on('roles')
-				->onUpdate('cascade')->onDelete('cascade');
+				$table->foreign('user_id')->references('id')->on('users')
+					->onUpdate('cascade')->onDelete('cascade');
+				$table->foreign('role_id')->references('id')->on('roles')
+					->onUpdate('cascade')->onDelete('cascade');
 
-			$table->primary(['user_id', 'role_id']);
-		});
+				$table->primary(['user_id', 'role_id']);
+			});
+		}
 
 		// Create table for storing permissions
-		Schema::create('permissions', function (Blueprint $table) {
-			$table->increments('id');
-			$table->string('name')->unique();
-			$table->string('display_name')->nullable();
-			$table->string('description')->nullable();
-			$table->timestamps();
-		});
+		if (Schema::hasTable('permissions')) {
+			Schema::create('permissions', function (Blueprint $table) {
+				$table->increments('id');
+				$table->string('name')->unique();
+				$table->string('display_name')->nullable();
+				$table->string('description')->nullable();
+				$table->timestamps();
+			});
+		}
 
 		// Create table for associating permissions to roles (Many-to-Many)
-		Schema::create('permission_role', function (Blueprint $table) {
-			$table->integer('permission_id')->unsigned();
-			$table->integer('role_id')->unsigned();
+		if (Schema::hasTable('permission_role')) {
+			Schema::create('permission_role', function (Blueprint $table) {
+				$table->integer('permission_id')->unsigned();
+				$table->integer('role_id')->unsigned();
 
-			$table->foreign('permission_id')->references('id')->on('permissions')
-				->onUpdate('cascade')->onDelete('cascade');
-			$table->foreign('role_id')->references('id')->on('roles')
-				->onUpdate('cascade')->onDelete('cascade');
+				$table->foreign('permission_id')->references('id')->on('permissions')
+					->onUpdate('cascade')->onDelete('cascade');
+				$table->foreign('role_id')->references('id')->on('roles')
+					->onUpdate('cascade')->onDelete('cascade');
 
-			$table->primary(['permission_id', 'role_id']);
-		});
-		Schema::table('roles', function (Blueprint $table) {
-			$table->unsignedInteger('company_id')->nullable()->after('id');
-			$table->unsignedMediumInteger('display_order')->default(0)->after('name');
-			$table->boolean('is_hidden')->after('description');
-			$table->unsignedInteger('created_by_id')->nullable()->after('is_hidden');
-			$table->unsignedInteger('updated_by_id')->nullable()->after('created_by_id');
-			$table->unsignedInteger('deleted_by_id')->nullable()->after('updated_by_id');
-			$table->softdeletes();
+				$table->primary(['permission_id', 'role_id']);
+			});
+		}
 
-			$table->foreign('created_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
-			$table->foreign('updated_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
-			$table->foreign('deleted_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
+		if (Schema::hasTable('roles')) {
+			Schema::table('roles', function (Blueprint $table) {
+				$table->unsignedInteger('company_id')->nullable()->after('id');
+				$table->unsignedMediumInteger('display_order')->default(0)->after('name');
+				$table->boolean('is_hidden')->after('description');
+				$table->unsignedInteger('created_by_id')->nullable()->after('is_hidden');
+				$table->unsignedInteger('updated_by_id')->nullable()->after('created_by_id');
+				$table->unsignedInteger('deleted_by_id')->nullable()->after('updated_by_id');
+				$table->softdeletes();
 
-		});
+				$table->foreign('created_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
+				$table->foreign('updated_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
+				$table->foreign('deleted_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
+			});
+		}
 	}
 
 	/**
