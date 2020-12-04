15a531533a4d6a90d519f41df9cf15be2d9e3f9a
Vijay-UITOUX <52068840+Vijay-UITOUX@users.noreply.github.com>
2020-12-04 17:59:01 +0530

    Update api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,19 +6,19 @@ use App\Http\Controllers\Api\Masters\Auth\RoleController;
 Route::group(['middleware' => ['api']], function () {
 	Route::group(['prefix' => '/api/master/auth/role'], function () {
 		$className = RoleController::class;
-		Route::get('index', $className.'@index');
-		Route::get('read/{id}', $className.'@read');
-		Route::post('save', $className.'@save');
-		Route::get('options', $className.'@options');
-		Route::get('delete/{User}', $className.'@delete');
+		Route::get('index', $className . '@index');
+		Route::get('read/{id}', $className . '@read');
+		Route::post('save', $className . '@save');
+		Route::get('options', $className . '@options');
+		Route::get('delete/{role}', $className . '@delete');
 	});
 
 	Route::group(['prefix' => '/api/master/auth/permission'], function () {
 		$className = PermissionController::class;
-		Route::get('index', $className.'@index');
-		Route::get('read/{id}', $className.'@read');
-		Route::post('save', $className.'@save');
-		Route::get('options', $className.'@options');
-		Route::get('delete/{User}', $className.'@delete');
+		Route::get('index', $className . '@index');
+		Route::get('read/{id}', $className . '@read');
+		Route::post('save', $className . '@save');
+		Route::get('options', $className . '@options');
+		Route::get('delete/{permission}', $className . '@delete');
 	});
 });
